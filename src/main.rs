e39c64c50675a363e920452d8f0a706d5787f703
Fristi <mark@vectos.net>
2023-10-27 08:54:18 +0200

    Add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
-use chrono::DateTime;
+use chrono::{DateTime, Utc};
 use crate::timeseries::Series;
 
 mod timeseries;
 
 fn main() {
 
-    let mut timeseries = Series::new(0.3f32);
+    let mut timeseries: Series<10, DateTime<Utc>, f32> = Series::new(0.3);
 
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T14:48:00.000Z").unwrap(), 32.6f32);
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T14:58:00.000Z").unwrap(), 32.7f32);
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T15:18:00.000Z").unwrap(), 32.5f32);
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T15:22:00.000Z").unwrap(), 33.8f32);
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T16:48:00.000Z").unwrap(), 34.0f32);
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T17:48:00.000Z").unwrap(), 28.2f32);
-    timeseries.append( DateTime::parse_from_rfc3339("2011-10-05T18:48:00.000Z").unwrap(), 12.3f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T14:48:00.000Z").unwrap().into(), 32.6f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T14:58:00.000Z").unwrap().into(), 32.7f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T15:18:00.000Z").unwrap().into(), 32.5f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T15:22:00.000Z").unwrap().into(), 33.8f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T16:48:00.000Z").unwrap().into(), 34.0f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T17:48:00.000Z").unwrap().into(), 28.2f32);
+    timeseries.append_monotonic( DateTime::parse_from_rfc3339("2011-10-05T18:48:00.000Z").unwrap().into(), 12.3f32);
+
+    let starts = timeseries.starts_at().unwrap();
+    let ends = timeseries.ends_at().unwrap();
+
+    let dur = ends.signed_duration_since(starts);
 
 
     println!("buckets: {:?}", timeseries.buckets);
+    println!("dur: {:?}", dur);
 }
--- src/timeseries.rs
+++ src/timeseries.rs
@@ -1,36 +1,74 @@
+use heapless::Vec;
 
-pub struct Series<I, T> {
-    max_deviation: T,
-    pub buckets: Vec<SerieEntry<I, T>>
+#[derive(Debug, Eq, PartialEq)]
+pub struct Series<const N : usize, I, T> {
+    pub max_deviation: T,
+    pub buckets: Vec<SerieEntry<I, T>, N>
 }
 
-impl <I : Ord, T : Deviate> Series<I, T> {
-    pub fn new(max_deviation: T) -> Series<I, T> {
+impl <const N : usize, I : Ord, T : Deviate> Series<N, I, T> {
+    pub fn new(max_deviation: T) -> Series<N, I, T> {
         Series { max_deviation, buckets: Vec::new() }
     }
 
-    pub fn append(&mut self, at: I, value: T) {
-        match self.buckets.pop() {
-            Some(v) => {
-                if v.value.deviate(&value, &self.max_deviation) {
-                    self.buckets.append(&mut vec![v, SerieEntry{ range: Range::new(at), value }])
-                } else {
-                    let new_range = v.range.extend(at);
-                    self.buckets.append(&mut vec![SerieEntry { range: new_range, value: v.value}])
+    pub fn append_monotonic(&mut self, at: I, value: T) -> bool {
+        if *&self.buckets.is_full() {
+            return false
+        } else {
+            return match self.buckets.pop() {
+                Some(v) => {
+                    let gt_start = &at > &v.range.start;
+                    let gt_end = &v.range.end.as_ref().map(|x| &at > x).unwrap_or(true);
+
+                    if gt_start && *gt_end {
+                        if v.value.deviate(&value, &self.max_deviation) {
+                            self.buckets.push(v);
+                            self.buckets.push(SerieEntry { range: Range::new(at), value });
+                        } else {
+                            let new_range = v.range.extend(at);
+                            self.buckets.push(SerieEntry { range: new_range, value: v.value });
+                        }
+                        true
+                    } else {
+                        self.buckets.push(v);
+                        false
+                    }
+                },
+                None => {
+                    self.buckets.push(SerieEntry { range: Range::new(at), value });
+                    true
                 }
-            },
-            None => self.buckets.append(&mut vec![SerieEntry{ range: Range::new(at), value }])
+            }
         }
+    }
+
+    pub fn starts_at(&self) -> Option<&I> {
+        self.buckets.first().map(|x| &x.range.start)
+    }
+
+    pub fn ends_at(&self) -> Option<&I> {
+        let mut end: Option<&I> = None;
+
+        for b in &self.buckets {
+            end = Some(&b.range.start).max(end);
+            end = b.range.end.as_ref().max(end);
+        }
+
+        return end
+    }
+
+    pub fn is_full(&self) -> bool {
+        return *&self.buckets.is_full()
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Range<I> {
     start: I,
     end: Option<I>
 }
 
-impl <I: Ord> Range<I> {
+impl <I: Ord + Sized> Range<I> {
     pub fn new(start: I) -> Range<I> {
         Range { start, end: None }
     }
@@ -43,7 +81,7 @@ impl <I: Ord> Range<I> {
     }
 }
 
-trait Deviate {
+pub trait Deviate {
     fn deviate(&self, other: &Self, max_deviation: &Self) -> bool;
 }
 
@@ -59,9 +97,122 @@ impl Deviate for f64 {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct SerieEntry<I, T> {
     range: Range<I>,
     value: T
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut timeseries: Series<10, u8, f32> = Series::new(0.3);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(timeseries.append_monotonic( 2_u8, 32.7f32));
+        assert!(timeseries.append_monotonic( 3_u8, 32.5f32));
+        assert!(timeseries.append_monotonic( 4_u8, 33.8f32));
+        assert!(timeseries.append_monotonic( 6_u8, 34.0f32));
+        assert!(timeseries.append_monotonic( 8_u8, 28.2f32));
+        assert!(timeseries.append_monotonic( 10_u8, 12.3f32));
+
+        let mut expected: Vec<SerieEntry<u8, f32>, 3> = Vec::new();
+
+        expected.push(SerieEntry { range: Range { start: 1, end: Some(6) }, value: 32.6 });
+        expected.push(SerieEntry { range: Range { start: 8, end: None }, value: 28.2 });
+        expected.push(SerieEntry { range: Range { start: 10, end: None }, value: 12.3 });
+
+        assert_eq!(timeseries.buckets, expected)
+    }
+
+    #[test]
+    fn should_not_append_when_full() {
+        let mut timeseries: Series<1, u8, f32> = Series::new(0.3);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(!timeseries.append_monotonic( 2_u8, 32.7f32));
+        assert!(timeseries.is_full());
+
+        let mut expected: Vec<SerieEntry<u8, f32>, 3> = Vec::new();
+
+        expected.push(SerieEntry { range: Range { start: 1, end: None }, value: 32.6 });
+
+        assert_eq!(timeseries.buckets, expected)
+    }
+
+
+    #[test]
+    fn should_return_false_when_append_is_not_monotonic_with_end() {
+        let mut timeseries: Series<10, u8, f32> = Series::new(0.3f32);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(timeseries.append_monotonic( 6_u8, 32.7f32));
+        assert!(!timeseries.append_monotonic( 1_u8, 32.5f32));
+
+        let mut expected: Vec<SerieEntry<u8, f32>, 3> = Vec::new();
+
+        expected.push(SerieEntry { range: Range { start: 1, end: Some(6) }, value: 32.6 });
+
+        assert_eq!(timeseries.buckets, expected)
+    }
+
+    #[test]
+    fn should_return_false_when_append_is_not_monotonic_without_end() {
+        let mut timeseries: Series<10, u8, f32> = Series::new(0.3f32);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(timeseries.append_monotonic( 6_u8, 35.7f32));
+        assert!(!timeseries.append_monotonic( 1_u8, 32.5f32));
+
+        let mut expected: Vec<SerieEntry<u8, f32>, 3> = Vec::new();
+
+        expected.push(SerieEntry { range: Range { start: 1, end: Some(6) }, value: 32.6 });
+
+        assert_eq!(timeseries.buckets, expected)
+    }
+
+    #[test]
+    fn starts_at_some() {
+        let mut timeseries: Series<10, u8, f32> = Series::new(0.3f32);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(timeseries.append_monotonic( 2_u8, 32.7f32));
+
+        assert_eq!(timeseries.starts_at(), Some(&1_u8));
+    }
+
+    #[test]
+    fn starts_at_none() {
+        let timeseries: Series<1, u8, f32> = Series::new(0.3f32);
+        assert_eq!(timeseries.starts_at(), None);
+    }
+
+    #[test]
+    fn ends_at_some_range_end() {
+        let mut timeseries: Series<10, u8, f32> = Series::new(0.3f32);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(timeseries.append_monotonic( 2_u8, 32.7f32));
+
+        assert_eq!(timeseries.ends_at(), Some(&2_u8));
+    }
+
+    #[test]
+    fn ends_at_some_range_start() {
+        let mut timeseries: Series<10, u8, f32> = Series::new(0.3f32);
+
+        assert!(timeseries.append_monotonic( 1_u8, 32.6f32));
+        assert!(timeseries.append_monotonic( 2_u8, 35.7f32));
+
+        assert_eq!(timeseries.ends_at(), Some(&2_u8));
+    }
+
+    #[test]
+    fn ends_at_none() {
+        let timeseries: Series<1, u8, f32> = Series::new(0.3f32);
+        assert_eq!(timeseries.ends_at(), None);
+    }
+}
